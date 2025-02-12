97441f566635b9c1b7d401bfe00f5f0ad1f718fd
VLADISLAV MOISEENKO <vlad981010@gmail.com>
2025-02-12 11:42:11 +0700

    feat(Clock.tsx) добавил контейнер для стрелок, +  сами стрелки и их размер (часовая стрелка в 2 раза меньше минутной), спозиционировал пока абсолютно.

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -3,14 +3,19 @@ import './Clock.css';
 
 const Clock: React.FC = () => {
   return (
-      <div className="clock">
-        {Array.from({ length: 12 }, (_, i) => (
-          <div className="number" key={i}>
-            <span>{i === 0 ? 12 : i}</span>
-          </div>
-        ))}
-        <div className="center-dot"></div>
+    <div className="clock">
+      {Array.from({ length: 12 }, (_, i) => (
+        <div className="number" key={i}>
+          <span>{i === 0 ? 12 : i}</span>
+        </div>
+      ))}
+      <div className="center-dot"></div>
+      <div className="hands-container">
+        <div className="hand hour-hand"></div>
+        <div className="hand minute-hand"></div>
+        <div className="hand second-hand"></div>
       </div>
+    </div>
   );
 };
 
